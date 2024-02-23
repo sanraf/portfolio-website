2953cf4f32bf9771a8ef09ad2fbdb0381c2689ff
sanraf <santoschrist123@gmail.com>
2024-02-24 01:42:57 +0200

    updated bottom navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import '../Styles/Footer.css'
 import facebookIcon from '../Assets/facebook.svg'
 import linkedinIcon from '../Assets/linkedin.svg'
 import githubIcon from '../Assets/icons8-github.svg'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
@@ -10,15 +11,17 @@ function Footer() {
               
             
             <div className="media-wrapper">
-                <div className="media-icon">
-                    <img src={facebookIcon} alt="facebook"/>
-                </div>
-                <div className="media-icon">
-                    <img src={githubIcon} alt="github"/>
-                </div>
-                <div className="media-icon">
-                    <img src={linkedinIcon} alt="linkedin"/>
-                </div>
+                <Link className="media-icon" target="_blank" rel="noopener noreferrer" >
+                        <img src={facebookIcon} alt="facebook"/>
+                </Link>
+
+                <Link className="media-icon" target="_blank" rel="noopener noreferrer" to={'https://github.com/sanraf?tab=repositories'}>
+                <img src={githubIcon} alt="github"/>
+                </Link>
+
+                <Link className="media-icon" target="_blank" rel="noopener noreferrer" >
+                <img src={linkedinIcon} alt="linkedin"/>
+                </Link>
             </div>
 
             <div className="copy-right">
